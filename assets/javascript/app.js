f89c0d1fc0bc4942cd464035f6551ed5775511d6
Yewbdar <Yewbdar.grima@gmail.com>
2018-03-14 14:36:14 -0500

    final work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     var config = {
         apiKey: "AIzaSyA2_wUh44MN8bkLfWV2OZWTHyIwh38EuTs",
         authDomain: "first-project-e0694.firebaseapp.com",
@@ -9,9 +9,10 @@ $(document).ready(function () {
         messagingSenderId: "837949321578"
     };
     firebase.initializeApp(config);
-   // console.log(firebase);
+    // console.log(firebase);
     var db = firebase.database();
     var ref = db.ref('trainSchedule');
+
     // var providerGoogle = new firebase.auth.GoogleAuthProvider();
     var providerGithub = new firebase.auth.GithubAuthProvider();
     //console.log(ref);
@@ -21,34 +22,21 @@ $(document).ready(function () {
         destination: null,
         firstTrainTimtime: null,
         frequency: 0,
-        waittingTime:0,
-        arrivalTime:0,
-        isUpdate:false,
-        selectedKey:null,
+
     };
-    // firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // ...
-    //   }).catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    //   });
-      firebase.auth().signInWithPopup(providerGithub).then(function(result) {
+    var calculatObj = {
+        waittingTime: 0,
+        arrivalTime: 0,
+        isUpdate: false,
+        selectedKey: null,
+    }
+    firebase.auth().signInWithPopup(providerGithub).then(function (result) {
         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         // ...
-      }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -57,174 +45,186 @@ $(document).ready(function () {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-      });
+    });
 
-      firebase.auth().signOut().then(function() {
+    firebase.auth().signOut().then(function () {
         // Sign-out successful.
-      }).catch(function(error) {
+    }).catch(function (error) {
         // An error happened.
-      });
-    
-    displayInTable();
-    //accept all user data and run calculteWattingTime function to get watting time ,
-    //and add object to firbase if rainObj.isUpdate is flase or update object to firbase if rainObj.isUpdate is true.
+    });
+
+    //accept all user data ,
+    //and add object to firbase if isUpdate is flase or update  if isUpdate is true.
     $("#addBtn").on("click", function () {
-        
-       trainObj.trainName = $("#train-name").val();
-        trainObj.destination = $("#dastination").val();
-        trainObj.frequency = $("#frequency").val();
-        trainObj.firstTrainTimtime=$("#time").val();
- 
-        calculateWattingTime(trainObj.firstTrainTimtime,trainObj.frequency);
-   //add new data in firbase 
-       if(!trainObj.isUpdate){
-        
-        ref.push(trainObj);
-    }
-    //update firbase data by using selectedkey  
-    else if(trainObj.isUpdate){ 
-          ref.child(trainObj.selectedKey).update(trainObj);
-          trainObj.isUpdate=false;
-    }
-        
+
+        // //add new data in firbase 
+        if (validation()) {
+            trainObj.trainName = $("#train-name").val();
+            trainObj.destination = $("#dastination").val();
+            trainObj.frequency = $("#frequency").val();
+            trainObj.firstTrainTimtime = $("#time").val();
+            if (!calculatObj.isUpdate) {
+
+                ref.push(trainObj);
+            }
+            //update firbase data by using selectedkey  
+            else if (calculatObj.isUpdate) {
+                ref.child(calculatObj.selectedKey).update(trainObj);
+                calculatObj.isUpdate = false;
+            }
+        }
     });
-    
+
     //get snapshot from firebase as an object and change it to array and store array elements in table  
-    function displayInTable(){
-        
-        ref.on('value', function (snapshot) {
-            
-             // chack if there is  a snapshot 
-            if (typeof Object.keys(snapshot) === 'undefined' ||  Object.keys(snapshot).length === 0 || snapshot === null ) {
-               
-                return;
-            }
-            var trainData = snapshot.val();
-            //console.log(trainSchedule);
-            var keys = Object.keys(trainData);
-            //console.log(keys);
-            $('#table').find('tbody').empty();
-            keys.forEach(element => {
 
-                var row = $("<tr>");
-                var updatBtn=$("<button>");
-                var removeBtn=$("<button>");
-                updatBtn.addClass("update btn-success");
-                removeBtn.addClass("remove btn-success ml-2");
-                updatBtn.text("Update");
-                removeBtn.text("Remove");
-                updatBtn.attr("value",element);//give an element(object name) as a value , it will use for update.
-                removeBtn.attr("value",element);//give an element(object name) as a value , it will use for remove.
-                var col = $("<td>");
-                var col2 = $("<td>");
-                var col3 = $("<td>");
-                var col4 = $("<td>");
-                var col5 = $("<td>");
-                var col6 = $("<td>");
-                
-                col.text(trainData[element].trainName);
-                col2.text(trainData[element].destination);
-                col3.text(trainData[element].frequency);
-                col4.text(trainData[element].arrivalTime);
-                col5.text(trainData[element].waittingTime);
-                col6.append(updatBtn,removeBtn);
-                row.append(col);
-                row.append(col2);
-                row.append(col3);
-                row.append(col4);
-                row.append(col5);
-                row.append(col6);
-                $("#tbody").prepend(row);
-            });
-           
-        // }
+    var trainData = null;
+    ref.on('value', function (snapshot) {
+        debugger
+        // chack if there is  a snapshot 
+        if (typeof Object.keys(snapshot) === 'undefined' || Object.keys(snapshot).length === 0 || snapshot === null) {
+
+            return;
+        }
+        trainData = snapshot.val();
+        //console.log(trainSchedule);
+        var keys = Object.keys(trainData);
+        //console.log(keys);
+        $('#table').find('tbody').empty();
+        keys.forEach(element => {
+
+            var row = $("<tr>");
+            var updatBtn = $("<button>");
+            var removeBtn = $("<button>");
+            updatBtn.addClass("update btn-success");
+            removeBtn.addClass("remove btn-success ml-2");
+            updatBtn.text("Update");
+            removeBtn.text("Remove");
+            updatBtn.attr("value", element);//give an element(object name) as a value , it will use for update.
+            removeBtn.attr("value", element);//give an element(object name) as a value , it will use for remove.
+            var col = $("<td>");
+            var col2 = $("<td>");
+            var col3 = $("<td>");
+            var col4 = $("<td>");
+            var col5 = $("<td>");
+            var col6 = $("<td>");
+            // console.log(trainData[element].currentTime);
+            calculateWattingTime(trainData[element].firstTrainTimtime, trainData[element].frequency)
+            col.text(trainData[element].trainName);
+            col2.text(trainData[element].destination);
+            col3.text(trainData[element].frequency);
+            col4.text(calculatObj.arrivalTime);
+            col5.text(calculatObj.waittingTime);
+            col6.append(updatBtn, removeBtn);
+            row.append(col);
+            row.append(col2);
+            row.append(col3);
+            row.append(col4);
+            row.append(col5);
+            row.append(col6);
+            $("#tbody").prepend(row);
         });
-        
-    }
-        //accepts minutes and change to hours
-         function convertMintsToHrs(minutes) {
-           
-            var h = Math.floor(minutes / 60);
-            var m = minutes % 60;
-            h = h < 10 ? '0' + h : h;
-            m = m < 10 ? '0' + m : m;
-            return (h > 12) ? h-12+":"+m+" P.M." : h+":"+m+" A.M.";
-       
-          }
-        function calculateWattingTime(time,frq){
-           
-            var date=new Date()
-            let currentTime=changeToMin(date.getHours() + ":" + date.getMinutes());
 
-            let startTime = changeToMin(time);
-           // debugger;
-            let arrivalTime=0;
-            
-            let y = (currentTime - startTime) % frq;
-            
-            if(y===0){
-                trainObj.waittingTime=0;
-                trainObj.arrivalTime=currentTime;
-            } else{
-                y =Math.floor ( (currentTime - startTime) /  frq ) + 1;
-                trainObj.waittingTime =((y*frq)+startTime) - currentTime;
-                trainObj.arrivalTime= (y*frq + startTime);
-                trainObj.arrivalTime=convertMintsToHrs(trainObj.arrivalTime) 
-            }
-           
-        }
-        // accepts hours (hh:mm) and change it to minutes
-        function changeToMin(time){
-             let timeSplit = time.split(':');
-             let hours = parseInt(timeSplit[0]);
-             let minutes = parseInt(timeSplit[1]); 
-               return (hours*60) + minutes;
-        }
-        //get snapshot from firbase and lode in the form by using selectedkey 
-        $(document).on("click",".update",function(){
-          
-          trainObj.isUpdate=true;
-           trainObj.selectedKey=$(this).val();//get updated element(object name) form button value
-          ref.on('value', function (snapshot) {
-            //chack if there is  a snapshot 
-            if (typeof Object.keys(snapshot) === 'undefined' ||  Object.keys(snapshot).length === 0 || snapshot === null ) {
-               
-                return;
-            }
-            var trainData=snapshot.val();
-            var keys=Object.keys(trainData);
-             keys.forEach(element => {
 
-                if(element === trainObj.selectedKey){
-                    $("#train-name").val(trainData[element].trainName);
-                    $("#dastination").val(trainData[element].destination);
-                    
-                  $("#frequency").val(trainData[element].frequency);
-                    
-                  $("#time").val(trainData[element].firstTrainTimtime);
-
-                }
-            });
-
-        
-        })   
     });
-    //remove data from firbase 
-    $(document).on("click",".remove",function(){
-       // debugger;
-        var key =$(this).val();
-        ref.child(key).remove();
+
+    function calculateWattingTime(time, frq) {
+
+
+        var a = moment(time, 'hh:mm').minutes();
+        var firstTime = moment.unix(a);
+
+        var currentTime = moment();
+
+        minuteDiffernce = currentTime.diff(firstTime, "minutes");
+        remainingTime = minuteDiffernce % frq;
+
+        calculatObj.waittingTime = frq - remainingTime;
+        calculatObj.arrivalTime = moment().add(calculatObj.waittingTime, "minute").format("hh:mm: A");
+
+    }
+    //get snapshot from firbase and lode in the form by using selectedkey 
+    $(document).on("click", ".update", function () {
+        debugger;
+        calculatObj.isUpdate = true;
+        calculatObj.selectedKey = $(this).val();
+        if (trainData === null) {
+
+            return;
+        }
+        var keys = Object.keys(trainData);
+        keys.forEach(element => {
+
+            if (element === calculatObj.selectedKey) {
+                $("#train-name").val(trainData[element].trainName);
+                $("#dastination").val(trainData[element].destination);
+
+                $("#frequency").val(trainData[element].frequency);
+
+                $("#time").val(trainData[element].firstTrainTimtime);
+
+            }
+        });
+
 
     })
-    $("#clearBtn").on("click",function(){
+});
+function validation() {
+    debugger;
+    var valid = true;
+    var newreg = /^(([0-1][0-9])|(2[0-3])):[0-5][0-9]$/;
+    var time = $("#time").val();
+    if ($("#train-name").val() === "") {
+        $("#name-validatin").text("please insert a train name !").css("color", "red")
 
-        $("#train-name").val("");
-        $("#dastination").val("");
-        
-      $("#frequency").val("");
-        
-     // $("#time").val(trainData[element].firstTrainTimtime);
+        valid = false
+    }
+    else if ($("#dastination").val() === "") {
+        $("#dastination-validatin").text("please insert a train destination !").css("color", "red")
+        valid = false
+    }
+    else if ($("#time").val() === "") {
+        $("#time-validatin").text("please insert a train start time !").css("color", "red")
+        valid = false
+    }
 
+    else if (!$("#time").val().match(newreg)) {
+        $("#time-validatin").text("Invalid time format , The valid format is hh:mm ").css("color", "red");
+        valid = false;
+    }
+    else if ($("#frequency").val() === "") {
+        $("#frequency-validatin").text("please insert a frequency !").css("color", "red")
+        valid = false
 
-    });
+    }
+    return valid;
+
+}
+//remove data from firbase 
+$(document).on("click", ".remove", function () {
+    // debugger;
+    var db = firebase.database();
+
+    var ref = db.ref('trainSchedule');
+
+    var key = $(this).val();
+    ref.child(key).remove();
+
 })
+$("#clearBtn").on("click", function () {
+
+    $("#train-name").val("");
+    $("#dastination").val("");
+
+    $("#frequency").val("");
+
+   $("#time").val("00:00");
+
+
+});
+//remove validation text 
+$(".form-control").keyup(function(){
+      $("p").text("");
+
+
+})
+
